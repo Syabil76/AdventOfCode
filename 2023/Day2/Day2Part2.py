8c040a712d93773c4d3114d984872ddc112f64bb
Syabil76 <142871006+Syabil76@users.noreply.github.com>
2024-07-24 15:50:14 +0800

    Create Day2Part2.py

---
+++ 2023/Day2/Day2Part2.py
@@ -0,0 +1,23 @@
+from file import list
+
+def main():
+    colors = ["red", "green", "blue"]
+    total = 0
+    for game in range(len(list)):
+        score = [0, 0, 0]
+        current = list[game]
+        for step in range(len(current)):
+            if current[step:step+1].isdigit() == True and current[step+1:step+2].isdigit() == False:
+                for i in range(len(colors)):
+                    if current[step+2:step+2+len(colors[i])] == colors[i] and int(current[step:step+1]) > score[i]:
+                        score[i] = int(current[step:step+1])
+            if current[step:step+2].isdigit() == True:
+                for j in range(len(colors)):
+                    if current[step+3:step+3+len(colors[j])] == colors[j] and int(current[step:step+2]) > score[j]:
+                        score[j] = int(current[step:step+2])
+        print(score)
+        total = total + score[0]*score[1]*score[2]
+        print(total)
+main()
+
+                                  
