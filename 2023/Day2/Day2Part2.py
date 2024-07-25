b4abe09e5fe8bc38c80c605064f738a7359c7da9
Syabil76 <142871006+Syabil76@users.noreply.github.com>
2024-07-25 16:00:46 +0800

    Update Day2Part2.py

--- 2023/Day2/Day2Part2.py
+++ 2023/Day2/Day2Part2.py
@@ -4,13 +4,16 @@ def main():
     colors = ["red", "green", "blue"]
     total = 0
     for game in range(len(list)):
+        #use a list to store MAXIMUM POSSIBLE number
         score = [0, 0, 0]
         current = list[game]
         for step in range(len(current)):
+            #finds highest possible single digit number 
             if current[step:step+1].isdigit() == True and current[step+1:step+2].isdigit() == False:
                 for i in range(len(colors)):
                     if current[step+2:step+2+len(colors[i])] == colors[i] and int(current[step:step+1]) > score[i]:
                         score[i] = int(current[step:step+1])
+            #finds highest possible two digit number
             if current[step:step+2].isdigit() == True:
                 for j in range(len(colors)):
                     if current[step+3:step+3+len(colors[j])] == colors[j] and int(current[step:step+2]) > score[j]:
