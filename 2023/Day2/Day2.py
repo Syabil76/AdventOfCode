c67dd5cde06a350fd107b4d11d62bb0b9b1ab07f
Syabil76 <142871006+Syabil76@users.noreply.github.com>
2024-07-25 15:58:37 +0800

    Update Day2.py

--- 2023/Day2/Day2.py
+++ 2023/Day2/Day2.py
@@ -9,14 +9,18 @@ def main():
         "blue": 14,
     }
     total =5050
+    #sets total to maximum amount and subtracts it when itf finds a number
     for game in range(len(list)):
         flag = False
         current = list[game]
         for step in range(len(current)):
+            #checks if its a two digit number as maximum values can ONLY be 2 digits
             if current[step:step+1].isdigit() == True:
                 if current[step+1:step+2].isdigit() == True:
                     if int(current[step:step+2]) > parameters["red"]:
+                        #checks if its above the lowest possible parameter
                         for x, y in parameters.items():
+                            #finds the color and checks if it fits the parameter and sets of a flag that ends the loop
                             if current[step+3:step+3+len(x)] == x:
                                 if int(current[step:step+2]) > y:
                                     no.append(f'game {game+1}')
