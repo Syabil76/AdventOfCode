6b4f1f56915d34906907f410f234b6bab5154c30
Syabil76 <142871006+Syabil76@users.noreply.github.com>
2024-07-25 15:56:10 +0800

    Update main.py

--- 2023/Day1/Part2/main.py
+++ 2023/Day1/Part2/main.py
@@ -20,11 +20,13 @@ def main():
         flag1 = False
         flag2 = False
         string = lister[i]
+        #does a 'race' to see if a digit or a word within dictionary comes first
         for j in range(len(string)):
             if string[j:j+1].isdigit() == True:
                 val1 = int(string[j:j+1])
                 break
             else:
+                #checks if its a valid word in dictionary (iterates through all possible values off of position alone)
                 for x, y in valid.items():
                     if string[j:j+len(x)] == x:
                         val1 = y
@@ -32,6 +34,7 @@ def main():
                         break
             if flag1 == True:
                 break
+        #sees if string at the end is char/number (iterate through dict if char)
         for k in range(len(string)):
             if string[len(string)-k:len(string)-k+1].isdigit() == True:
                 val2 = int(string[len(string)-k:len(string)-k+1])
@@ -44,6 +47,7 @@ def main():
                         break
             if flag2 == True:
                 break          
+            #just used math instead of stringing it together
         total = total + val1*10 + val2
         print(total)
 
