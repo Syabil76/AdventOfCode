894ccb071d3191e341b11c3badc0461e42ee66cb
Syabil76 <142871006+Syabil76@users.noreply.github.com>
2024-07-29 15:52:35 +0800

    Update and rename 2ndSolution to 2ndSolution.py

---
+++ 2023/Day3/2ndSolution.py
@@ -0,0 +1,50 @@
+with open("final.txt") as file:
+    data = file.read()
+    text = data.strip().split("\n")
+
+def main():
+    final = 0
+    for r in range(len(text)):
+        cl = text[r]
+        i = 0
+        while i < len(cl):
+            if cl[i:i+1].isdigit() == True:
+                a = checkdigit(i ,cl)[0]
+                num = checkdigit(i ,cl)[1]
+                final = final + lookaround(i, r, a, cl, num)
+                
+                i = i + a
+            i = i + 1
+    print(final)
+
+def checkdigit(i, cl):
+    a = 0
+    while True:
+        if cl[i:i+a+1].isdigit() == True and cl[i:i+a+1] != '.':
+            a = a + 1
+        else:
+            num = int(cl[i:i+a])
+            return a, num
+
+
+def lookaround(i, r, a, cl, num):
+    x = i-1
+    y = i+1+a
+    z = r-1
+    w = r+1
+    if i < 1:
+        x = i
+    if i > len(cl) - 2:
+        y = i+a
+    if r < 1:
+        z = r
+    if r > len(text) - 2:
+        w = r
+    print(f"{num} | {x, y, z, w}")
+    for j in range(z, w+1):
+        for k in range(x, y):
+            if text[j][k].isdigit() == False and text[j][k] != '.':
+                return num
+    return 0
+main()
+
